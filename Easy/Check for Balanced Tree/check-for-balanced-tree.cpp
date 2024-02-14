ebef91b9b446e394c9db80f3316cc41f0e7e4bce
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-15 00:14:16 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check for Balanced Tree/check-for-balanced-tree.cpp
+++ Easy/Check for Balanced Tree/check-for-balanced-tree.cpp
@@ -109,21 +109,31 @@ class Solution{
     }
     public:
     //Function to check whether a binary tree is balanced or not.
+    pair<bool, int> isBalancedFast(Node *root) {
+        // base case
+        if(root == nullptr) {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        pair<bool ,int> left = isBalancedFast(root->left);
+        pair<bool, int> right = isBalancedFast(root->right);
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        bool diff = abs(left.second-right.second) <= 1;
+        
+        pair<bool, int> ans;
+        ans.first = (leftAns && rightAns && diff);
+        ans.second = max(left.second, right.second) + 1;
+        
+        return ans;
+    }
+    
     bool isBalanced(Node *root)
     {
         // base case
-        if(root == nullptr) return true;
-        
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        
-        bool diff = abs(height(root->left) - height(root->right)) <= 1;
-        
-        if(left && right && diff) {
-            return true;
-        }
-        
-        return false;
+        return isBalancedFast(root).first;
         
     }
 };
