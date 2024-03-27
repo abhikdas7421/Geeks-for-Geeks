1f5060e6e7669a4976aeda2f2e125ecb70b50b2a
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-27 23:37:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
+    vector<long long> ans;
+    deque<long long> dque;
+    
+    for(long long int i = 0; i < K; i++) {
+        if(A[i] < 0) {
+            dque.push_back(i);
+        }
+    }
+    
+    if(!dque.empty()) {
+        ans.push_back(A[dque.front()]);
+    }
+    else {
+        ans.push_back(0);
+    }
+    
+    
+    int i = 0;
+    int j = K;
+    
+    while(j < N) {
+        
+        if(A[j] < 0) {
+            dque.push_back(j);
+        }
+        
+        i++;
+        j++;
+        
+        while(!dque.empty() && dque.front() < i) {
+            dque.pop_front();
+        }
+        
+        if(!dque.empty()) {
+            ans.push_back(A[dque.front()]);
+        }
+        else {
+            ans.push_back(0);
+        }
+        
+    }
+    
+    return ans;
+ }
