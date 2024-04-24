15a2bc8c6384a9d53792f1bf470d242ae0e8b7bf
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-25 01:54:15 +0530

    Updated Solution - GfG to GitHub

--- Easy/Common elements/common-elements.cpp
+++ Easy/Common elements/common-elements.cpp
@@ -16,24 +16,13 @@ class Solution
             
             while(i < n1 && j < n2 && k < n3) {
                 if(A[i] == B[j] && B[j] == C[k]) {
+                    if(ans.empty() || ans.back() != A[i]) ans.push_back(A[i]);
                     
-                    if(ans.empty() || ans.back() != A[i]) {
-                        ans.push_back(A[i]);
-                    }
-                    
-                    i++;
-                    j++;
-                    k++;
+                    i++; j++; k++;
                 }
-                else if(A[i] < B[j] && A[i] < C[k]) {
-                    i++;
-                }
-                else if(B[j] < C[k]) {
-                    j++;
-                }
-                else {
-                    k++;
-                }
+                else if(A[i] < B[j] && A[i] < C[k]) i++;
+                else if(B[j] < C[k]) j++;
+                else k++;
             }
             
             return ans;
